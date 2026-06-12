428a1d26fe3278ff87023250efb4d088d52c42e4
KageWatch <otakustream.admin@gmail.com>
2026-06-12 18:30:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def m3u8_proxy(url: str, referer: str = ""):
                 else:
                     # 🔴 FIX 2: Proxy the actual video chunk (.ts) lines
                     abs_url = urljoin(url, line)
-                    proxy_link = proxy_link = f"/proxy?url={quote(abs_url)}&referer={quote(referer)}"
+                    proxy_link = f"https://kagewatch-proxy.onrender.com/proxy?url={quote(abs_url)}&referer={quote(referer)}"
                     new_content += proxy_link + "\n"
                     
             return Response(content=new_content, media_type="application/vnd.apple.mpegurl")
